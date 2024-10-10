9b9cdc06b757e51d959cb0c540b15c23eeb6460f
ggudok <masagudkova0@gmail.com>
2024-10-10 16:43:16 +0700

    Initial Commit

---
+++ first_lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_lesson
+//
+//  Created by Маша Гудкова on 10.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
